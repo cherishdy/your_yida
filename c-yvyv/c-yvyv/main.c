50dfc99963905a6036e45445994c192dc525c39f
Rdxer <240302832@qq.com>
2015-07-19 23:38:57 +0800

    这是我添加的代码文件~
    xxxx

---
+++ c-yvyv/c-yvyv/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  c-yvyv
+//
+//  Created by Xiaofeng Li  on 15/7/19.
+//  Copyright (c) 2015年 Xiaofeng Li . All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
